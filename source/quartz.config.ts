7bfd3d81888612c225017876c383d280ebbcb6e5
ktwindmill <62659482+ktwindmill@users.noreply.github.com>
2025-11-14 15:38:50 +0000

    more

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Private"],
+    ignorePatterns: ["private", "templates", ".obsidian", "**/private"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
